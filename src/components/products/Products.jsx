481eb8f5fb1976556536ab8192c979942a9c0bb8
Timilehin Bakare <timmybaks2002@gmail.com>
2023-01-17 00:56:20 +0100

    create grid for products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 
+const products = [
+    {id: 1, name: 'Shoes', description: 'Running Shoes'},
+    {id: 2, name: 'MacBook', description: 'Apple'},
+]
+
+
 const Products = () =>{
     <main>
-        
+        <Grid container justify="center" spacing={4}>
+        {products.map(product =>(
+            <Grid item key={product.id} xs={12} sm={6} lg={3}>
+                <Product product={product}/>
+            </Grid>
+        ))}
+
+        </Grid>
     </main>
 }
 
---
+++ src/components/products/product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Card, CardMedia, CardContent, CardActions, Typography, IconBottom } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+
+const Product = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Product
