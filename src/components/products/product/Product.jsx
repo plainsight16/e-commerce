d9bfd351ce16c807649ec71e9992bbd3fe9123f9
Timilehin Bakare <timmybaks2002@gmail.com>
2023-01-23 17:47:18 +0100

    include product content

--- src/components/products/product/Product.jsx
+++ src/components/products/product/Product.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconBottom } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
 
-const Product = () => {
+const Product = ({product}) => {
   return (
-    <div>
-      
-    </div>
+    <Card className={classes.root}>
+      <CardMedia className={classes.cardMedia} image=''></CardMedia>
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant='h5' gutterBottom>{Product.name}</Typography>
+          <Typography variant='h5'>{Product.price}</Typography>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 
